dd070a5d584a3a304d2d9db121ad5d9a4c934c08
Jan Hansen <tiujanhansen@gmail.com>
2026-02-17 17:54:39 +0800

    feat: added error handler for axum and io

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,12 +2,15 @@ use tracing::{info};
 use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 use axum::Router;
 use axum::routing::get;
+use crate::error::Error;
+
+mod error;
 
 async fn health_check() -> impl axum::response::IntoResponse
 {
     println!("--> {:<12} - Health Check -" , "HANDLER");
 
-    axum::response::Html(format!("<h1> The server is definitely running </h1>"))
+    axum::response::Html("<h1> The server is definitely running </h1>")
 }
 
 fn router() -> Router
@@ -16,8 +19,9 @@ fn router() -> Router
         .route("/", get(health_check)) 
 }
 
+type MainError<T> = Result<T, Error>;
 #[tokio::main]
-async fn main()
+async fn main() -> MainError<()>
 {
     //This is boilerplate code for sanity checks
     tracing_subscriber::registry()
@@ -30,10 +34,12 @@ async fn main()
     let app = router();
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:6969")
-        .await.expect("Listener failed");
+        .await?;
     
     info!("Server runnning on http://localhost:6969");
-    axum::serve(listener,app).await.expect("Failed to start server");
+    axum::serve(listener,app).await?;
 
     info!("Oh, this is gonna be fucking hell.");
+
+    Ok(())
 }
